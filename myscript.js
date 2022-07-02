58b543c02795e6a8f1a0f8b08d62290d41d98202
Adam Graves <adamgraves1207@gmail.com>
2022-07-01 20:07:07 -0700

    added round system to go to 5 and updated function to return correct variables

--- myscript.js
+++ myscript.js
@@ -1,3 +1,14 @@
+// let test = prompt("Enter something");
+// test = test.toLowerCase();
+// console.log(test);
+
+let playerCounter = 0;
+
+let computerCounter = 0;
+
+game();
+
+
 function computerPlay() {
     let arr = ["rock", "paper", "scissors"];
     return arr[Math.floor(Math.random() * arr.length)];
@@ -5,19 +16,55 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection == "rock" && computerSelection == "scissors") {
-        return "You beat the computer, nice!";
-    }else if(playerSelection == "paper" && computerSelection == "rock") {
-        return "You beat the computer, nice!";
-    }else if(playerSelection == "scissors" && computerSelection == "paper") {
-        return "You beat the computer, nice!";
-    }else {
+
+    if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        console.log(computerSelection);
+        console.log("You beat the computer in this round, nice!")
+        return playerCounter++;
+    }else if(playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        console.log(computerSelection);
+        console.log("You beat the computer in this round, nice!")
+        return playerCounter++;
+    }else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        console.log(computerSelection);
+        console.log("You beat the computer in this round, nice!")
+        return playerCounter++;
+    }else if(playerSelection.toLowerCase() == computerSelection) {
+        console.log(computerSelection);
+        console.log("It's a tie!")
+    }
+    else {
+        console.log(computerSelection);
         console.log("The computer wins this time, unlucky got mine.");
+        return computerCounter++;
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+function game() {
 
-console.log(playRound(playerSelection, computerSelection));
+    for(let i = 0; i < 5; i++) {
+
+        let computerSelection = computerPlay();
+
+        let playerSelection = prompt("Rock, Paper, or Scissors? Feeling lucky punk?");
+
+        playRound(playerSelection, computerSelection);
+    }
+
+    console.log("Here is the player count: " + playerCounter);
+    console.log("Here is the computer count: " + computerCounter);
+
+    if(playerCounter > computerCounter) {
+        console.log("You beat the computer in a emergency best of 5! Woot!");
+    }else if(playerCounter == computerCounter) {
+        console.log("Tied! Run it back!");
+    }
+    else {
+        console.log("The computer won this time... emergency best of 7?!");
+    }
+}
+
+
+
+
 
